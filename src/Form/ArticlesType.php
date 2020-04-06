497e5df4f95faffd8947bc7516346c46f9225366
Marylou OLIVIER <marylou.olivier.tomme@gmail.com>
2020-04-06 10:39:21 +0100

    Problème CRUD

--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -2,19 +2,33 @@
 
 namespace App\Form;
 
+use App\Entity\Fournisseurs;
 use App\Entity\Articles;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class ArticlesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('NomArt')
-            ->add('QtArt')
-            ->add('Fournisseur')
+            ->add('NomArt', null, [
+                'attr' => [
+                    'placeholder' => 'Nom article'
+                ]
+            ])
+            ->add('QtArt', null, [
+                'attr' => [
+                    'placeholder' => 'Quantité article'
+                ]
+            ])
+            ->add('Fournisseur', EntityType::class, [
+                'class' => Fournisseurs::class,
+                'choice_label' => 'NomFour'
+            ])
         ;
     }
 
